9685f84b54d0df954d7932b70851ced2802167f0
AlexFredrich <alexandra.fredrich@loop.colum.edu>
2018-04-11 11:48:09 -0500

    Character stats screen

--- Assets/Scripts/InventoryUIFunctionality.cs
+++ Assets/Scripts/InventoryUIFunctionality.cs
@@ -7,14 +7,15 @@ public class InventoryUIFunctionality : MonoBehaviour {
 
     //Fields for the UI screens aka panels
     [SerializeField]
-    private GameObject inventoryPanel, skillsPanel, mapPanel, questPanel, optionsPanel;
+    private GameObject inventoryPanel, characterPanel, skillsPanel, mapPanel, questPanel, optionsPanel;
     //Fields for the UI buttons
-    private Button inventoryButton, skillButton, mapButton, questButton, optionsButton;
+    private Button inventoryButton, characterButton, skillButton, mapButton, questButton, optionsButton;
 
 	// Use this for initialization
 	void Start ()
     {
         inventoryButton = GameObject.Find("Inventory").GetComponent<Button>();
+        characterButton = GameObject.Find("Character").GetComponent<Button>();
         skillButton = GameObject.Find("Skills").GetComponent<Button>();
         mapButton = GameObject.Find("Map").GetComponent<Button>();
         questButton = GameObject.Find("Quests").GetComponent<Button>();
@@ -32,28 +33,51 @@ public class InventoryUIFunctionality : MonoBehaviour {
     {
         //Setting the correct panel to appear
         inventoryPanel.SetActive(true);
+        characterPanel.SetActive(false);
         skillsPanel.SetActive(false);
         mapPanel.SetActive(false);
         questPanel.SetActive(false);
         optionsPanel.SetActive(false);
         //Making sure that buttons are interactable, but the current screens one is not
         inventoryButton.interactable = false;
+        characterButton.interactable = true;
         skillButton.interactable = true;
         mapButton.interactable = true;
         questButton.interactable = true;
         optionsButton.interactable = true;
     }
+
+    public void CharacterScreen()
+    {
+        //Setting the correct panel to appear
+        inventoryPanel.SetActive(false);
+        characterPanel.SetActive(true);
+        skillsPanel.SetActive(false);
+        mapPanel.SetActive(false);
+        questPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+        //Making sure that buttons are interactable, but the current screens one is not
+        inventoryButton.interactable = true;
+        characterButton.interactable = false;
+        skillButton.interactable = true;
+        mapButton.interactable = true;
+        questButton.interactable = true;
+        optionsButton.interactable = true;
+    }
+
     //When the skills/abilities button is pressed
     public void SkillsScreen()
     {
         //Setting the correct panel to appear
         inventoryPanel.SetActive(false);
+        characterPanel.SetActive(false);
         skillsPanel.SetActive(true);
         mapPanel.SetActive(false);
         questPanel.SetActive(false);
         optionsPanel.SetActive(false);
         //Making sure that buttons are interactable, but the current screens one is not
         inventoryButton.interactable = true;
+        characterButton.interactable = true;
         skillButton.interactable = false;
         mapButton.interactable = true;
         questButton.interactable = true;
@@ -64,12 +88,14 @@ public class InventoryUIFunctionality : MonoBehaviour {
     {
         //Setting the correct panel to appear
         inventoryPanel.SetActive(false);
+        characterPanel.SetActive(false);
         skillsPanel.SetActive(false);
         mapPanel.SetActive(true);
         questPanel.SetActive(false);
         optionsPanel.SetActive(false);
         //Making sure that buttons are interactable, but the current screens one is not
         inventoryButton.interactable = true;
+        characterButton.interactable = true;
         skillButton.interactable = true;
         mapButton.interactable = false;
         questButton.interactable = true;
@@ -80,12 +106,14 @@ public class InventoryUIFunctionality : MonoBehaviour {
     {
         //Setting the correct panel to appear
         inventoryPanel.SetActive(false);
+        characterPanel.SetActive(false);
         skillsPanel.SetActive(false);
         mapPanel.SetActive(false);
         questPanel.SetActive(true);
         optionsPanel.SetActive(false);
         //Making sure that buttons are interactable, but the current screens one is not
         inventoryButton.interactable = true;
+        characterButton.interactable = true;
         skillButton.interactable = true;
         mapButton.interactable = true;
         questButton.interactable = false;
@@ -96,12 +124,14 @@ public class InventoryUIFunctionality : MonoBehaviour {
     {
         //Setting the correct panel to appear
         inventoryPanel.SetActive(false);
+        characterPanel.SetActive(false);
         skillsPanel.SetActive(false);
         mapPanel.SetActive(false);
         questPanel.SetActive(false);
         optionsPanel.SetActive(true);
         //Making sure that buttons are interactable, but the current screens one is not
         inventoryButton.interactable = true;
+        characterButton.interactable = true;
         skillButton.interactable = true;
         mapButton.interactable = true;
         questButton.interactable = true;
