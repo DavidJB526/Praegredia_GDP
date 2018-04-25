e81b78f6ce70361289873cc13cdd01cd93a2cb26
apod1010 <32077837+apod1010@users.noreply.github.com>
2018-04-25 15:21:53 -0500

    Title Screen
    Added "Bull Skull" asset from asset store
    Added "White Smoke" particle effect from store
    Added title scene complete with animations
    Added basic functions for title buttons

---
+++ Assets/Scripts/Menus/TitleScreenManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void OnCreditsClick()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void OnQuitClick()
+    {
+        Application.Quit();
+    }
+}
